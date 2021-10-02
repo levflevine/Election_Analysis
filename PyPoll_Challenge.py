2a77619a0463b112c1520ad9a9a6018a6f41494a
levflevine <levflevine@gmail.com>
2021-10-01 20:41:01 -0400

    Final Challenge Submission

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -82,7 +82,7 @@ with open(file_to_save, "w") as txt_file:
 
     # Print the final vote count (to terminal)
     election_results = (
-        f"\nElection Results\n"
+        f"Election Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
         f"-------------------------\n\n"
